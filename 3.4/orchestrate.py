3617e554936a81d2bfcf66abc17a9a5f9faf466f
KonuTech <borowiec.k@gmail.com>
2023-06-08 10:36:58 +0200

    Editing for HW3

--- 3.4/orchestrate.py
+++ 3.4/orchestrate.py
@@ -110,8 +110,7 @@ def train_best_model(
 
 
 @flow
-# def main_flow(
-def test2_flow(
+def hw3_flow1(
     # train_path: str = "./data/green_tripdata_2021-01.parquet",
     # val_path: str = "./data/green_tripdata_2021-02.parquet",
     train_path: str = "./data/green_tripdata_2023-01.parquet",
@@ -135,4 +134,4 @@ def test2_flow(
 
 
 if __name__ == "__main__":
-    test2_flow()
+    hw3_flow1()
